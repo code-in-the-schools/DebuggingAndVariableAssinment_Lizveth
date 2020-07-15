21035d29b27700d32af01082c4e3bd16bdb6a047
LizvethM <morales-garciaLizveth2021@email.com>
2020-07-15 15:02:23 +0000

    DebuggingAndVariableAssinment_Lizveth

---
+++ main.py
@@ -0,0 +1,12 @@
+example = "example"
+print("This is an "+ example+ " and has no errors.")
+
+name = "Elia Deppe"
+fav_artist = "Earl Sweatshirt"
+vacation = "Nosara"
+hobby = "Painting & skateboarding"
+today = """06/20/22"""
+
+print("My name is "+ name+" and my favorite artist is "+ fav_artist)
+print("I love to go on vacation to "+ vacation+" and my two main hobbies are "+hobby)
+print("Today's date is: "+today)
